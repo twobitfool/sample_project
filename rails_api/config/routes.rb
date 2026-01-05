99d0889f9758b686dad6bddbd648659686cf67f1
Matt Smith <matt@twobitfool.com>
2026-01-04 19:42:01 -0500

    Add API routes for device readings

--- rails_api/config/routes.rb
+++ rails_api/config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "/ping", to: "application#ping"
+  post "/readings", to: "readings#create"
+  get "/devices/:id/latest_timestamp", to: "devices#latest_timestamp"
+  get "/devices/:id/total_count", to: "devices#total_count"
 end
